fa97d9eaf2adbd1b7e45f540f7f7d6f50789417d
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-04-17 02:11:01 +0700

    add stage 2

--- bloxorz/stages/stages.py
+++ bloxorz/stages/stages.py
@@ -24,5 +24,38 @@ def stage1():
     arr[5] = [None, None, None, None, None,
               None, Tile(), Tile(), Tile()]
 
-    s = Stage('stage1', arr, 2, 2)
+    s = Stage('stage1', arr, 1, 1)
     s.save('stage1')
+
+
+def stage2():
+    arr = [[] for i in range(6)]  # type: List[List[Tile]]
+    bridge_left = [Tile(T.bridge, False), Tile(T.bridge, False)]
+    bridge_right = [Tile(T.bridge, False), Tile(T.bridge, False)]
+
+    arr[0] = [None, None, None, None, None, None,
+              Tile(), Tile(), Tile(), Tile(),
+              None, None, Tile(), Tile(), Tile()]
+
+    arr[1] = [Tile(), Tile(), Tile(), Tile(), None, None,
+              Tile(), Tile(), Tile(T.hard_open_gate, bridge_right), Tile(),
+              None, None, Tile(), Tile(T.goal), Tile()]
+
+    arr[2] = [Tile(), Tile(), Tile(T.soft_gate, bridge_left), Tile(),
+              None, None, Tile(), Tile(), Tile(), Tile(),
+              None, None, Tile(), Tile(), Tile()]
+
+    arr[3] = [Tile(), Tile(), Tile(), Tile(),
+              None, None, Tile(), Tile(), Tile(), Tile(),
+              None, None, Tile(), Tile(), Tile()]
+
+    arr[4] = [Tile(), Tile(), Tile(), Tile(),
+              bridge_left[0], bridge_left[1], Tile(), Tile(), Tile(), Tile(),
+              bridge_right[0], bridge_right[1], Tile(), Tile(), Tile()]
+
+    arr[5] = [Tile(), Tile(), Tile(), Tile(),
+              Tile(), Tile(), Tile(), Tile(),
+              None, None, None, None, None, None]
+
+    s = Stage('stage2', arr, 1, 4)
+    s.save('stage2')
