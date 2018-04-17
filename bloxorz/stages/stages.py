45edcb72051ffda186a6f0c329bc39dc5ec1e1bc
huylenguyen806 <huylenguyen806@github.com>
2018-04-17 11:47:16 +0700

    add stages 3,4,5

--- bloxorz/stages/stages.py
+++ bloxorz/stages/stages.py
@@ -26,3 +26,96 @@ def stage1():
 
     s = Stage('stage1', arr, 2, 2)
     s.save('stage1')
+
+
+def stage3():
+    arr = [[] for i in range(6)]
+
+    arr[0] = [None for i in range(6), Tile() for i in range(7)]
+
+    arr[1] = [Tile() for i in range(4), None, None, 
+              Tile() for i in range(3), None, None, Tile(), Tile()]
+
+    arr[2] = [Tile() for i in range(9), None, None, Tile() for i in range(4)]
+
+    arr[3] = [Tile() for i in range(4), None for i in range(7), 
+              Tile(), Tile(), Tile(T.goal), Tile()]
+
+    arr[4] = [Tile() for i in range(4), None for i in range(7), 
+              Tile(), Tile(), Tile(), Tile()]
+    
+    arr[5] = [None for i in range(12), Tile(), Tile(), Tile()]
+
+    s = Stage('stage3', arr, 1, 3)
+    s.save('stage3')
+
+
+def stage4():
+    arr = [[] for i in range(9)]
+
+    arr[0] = [None, None, None, Tile(T.soft_ground) for i in range(7)]
+
+    arr[1] = [None, None, None, Tile(T.soft_ground) for i in range(7)]
+
+    arr[2] = [Tile() for i in range(4), None for i in range(5), Tile() for i in range(3)]
+
+    arr[3] = [Tile(), Tile(), Tile(), None for i in range(7), Tile(), Tile()]
+
+    arr[4] = [Tile(), Tile(), Tile(), None for i in range(7), Tile(), Tile()]
+
+    arr[5] = [Tile(), Tile(), Tile(), None, None, 
+              Tile() for i in range(4), Tile(T.soft_ground) for i in range(5)]
+
+    arr[6] = [Tile(), Tile(), Tile(), None, None, 
+              Tile() for i in range(4), Tile(T.soft_ground) for i in range(5)]
+
+    arr[7] = [None for i in range(5), Tile(), Tile(T.goal), Tile(), None, None, 
+              Tile(T.soft_ground), Tile(T.soft_ground), Tile(), Tile(T.soft_ground)]
+
+    arr[8] = [None for i in range(5), Tile(), Tile(), Tile(), None, None, 
+              Tile(T.soft_ground) for i in range(4)]
+
+    s = Stage('stage4', arr, 1, 5)
+    s.save('stage4')
+
+
+def stage5():
+    B1 = [Tile(T.bridge, True), Tile(T.bridge, True)]
+    B2 = [Tile(T.bridge, True), Tile(T.bridge, True)]
+    B3 = [Tile(T.bridge, True), Tile(T.bridge, True)]
+    arr = [[] for i in range(10)]
+
+    arr[0] = [None for i in range(11)]
+    arr[0] += [Tile() for i in range(4)]
+
+    arr[1] = [None, Tile(), Tile(), Tile(), Tile(), B1[0], B1[1], 
+              Tile(), Tile(T.soft_button, B1)]
+    arr[1] += [Tile() for i in range(6)]
+
+    arr[2] = [None, Tile() for i in range(4)]
+    arr[2] += [None for i in range(7)]
+    arr[2] += [Tile() for i in range(3)]
+
+    arr[3] = [None, Tile(), Tile(), Tile(T.soft_special_button, B3, []), Tile()]
+
+    arr[4] = [None, Tile() for i in range(4)]
+
+    arr[5] = [None, None, None, Tile(), Tile(), Tile(), 
+              Tile(T.soft_special_button, [], B3), Tile(), B2[0], B2[1], 
+              Tile(), Tile(), Tile()]
+    
+    arr[6] = [None for i in range(10)]
+    arr[6] += [Tile() for i in range(4)]
+    arr[6].append(Tile(T.soft_special_button, B3, []))
+
+    arr[7] = [Tile() for i in range(3)]
+    arr[7] += [None for i in range(7)]
+    arr[7] += [Tile() for i in range(5)]
+
+    arr[8] = [Tile(), Tile(T.goal), Tile() for i in range(3), B3[0], B3[1]]
+    arr[8] += [Tile() for i in range(6)]
+
+    arr[9] = [Tile() for i in range(4)]
+
+    s = Stage('stage5', arr, 1, 13)
+    s.save('stage5')
