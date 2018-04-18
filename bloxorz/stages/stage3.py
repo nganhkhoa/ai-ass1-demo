b39586df3094495d3723b348da14421e1186d087
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-04-19 02:53:11 +0700

    fix start index

--- bloxorz/stages/stage3.py
+++ bloxorz/stages/stage3.py
@@ -25,5 +25,5 @@ def stage3():
 
     arr[5] = [None for i in range(12)] + [Tile(), Tile(), Tile()]
 
-    s = Stage('stage3', arr, 1, 3)
+    s = Stage('stage3', arr, 3, 1)
     s.save('stage3')
--- bloxorz/stages/stage5.py
+++ bloxorz/stages/stage5.py
@@ -45,5 +45,5 @@ def stage5():
 
     arr[9] = [Tile() for i in range(4)]
 
-    s = Stage('stage5', arr, 1, 13)
+    s = Stage('stage5', arr, 13, 1)
     s.save('stage5')
