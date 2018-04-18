0b7a629945b1729647dcbd575c206922b3540c08
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-04-19 02:51:08 +0700

    add reverse move

--- bloxorz/solver/moves.py
+++ bloxorz/solver/moves.py
@@ -6,3 +6,9 @@ class moves(Enum):
     left = 3
     up = 1
     down = 2
+
+    def reverse(self):
+        if self.value % 2 == 0:
+            return moves(self.value - 1)
+        else:
+            return moves(self.value + 1)
