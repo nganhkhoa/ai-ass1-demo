6b4a3659f8c0d07fa54c0832e94bb4aa409a5b09
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-04-19 19:33:57 +0700

    BFS template

--- bloxorz/solver/BFS.py
+++ bloxorz/solver/BFS.py
@@ -1,43 +1,79 @@
 from bloxorz.solver.moves import moves
+from bloxorz.solver.State import move
 from queue import Queue
+from copy import deepcopy
 
 def BFS(s):
-    print("[+] Looping...")
-    q = queue.Queue()
-    start_block = Block(1, 1)
-    start_path = [board]
-    start_node = Node(start_path, start_block)
-    q.put(start_node)
-    while not q.empty():
-        top = q.get()
-        if top.block.location[0] == goal[0] and top.block.location[1] == goal[
-            1] and top.block.state == State.standingBlock:
-            for i in range(len(top.path)):
-                print(top.path[i])
-            break
-        block = top.block
-        m = top.path[-1]
-        if block.can_move_left(m):
-            t = list(m)
-            b = copy.deepcopy(block)
-            b.move_left(t)
-            p = list(top.path).append(t)
-            temp = Node(p, b)
-        if block.can_move_right(m):
-            t = list(m)
-            b = copy.deepcopy(block)
-            b.move_right(t)
-            p = list(top.path).append(t)
-            temp = Node(p, b)
-        if block.can_move_up(m):
-            t = list(m)
-            b = copy.deepcopy(block)
-            b.move_up(t)
-            p = list(top.path).append(t)
-            temp = Node(p, b)
-        if block.can_move_down(m):
-            t = list(m)
-            b = copy.deepcopy(block)
-            b.move_down(t)
-            p = list(top.path).append(t)
-            temp = Node(p, b)
+    queue = s.queue
+
+    loop = 1
+    while len(queue) != 0 and loop <= 50000:
+        try:
+            print("[+] Looping {:5d} / {}".format(loop, 50000), end='\r')
+            loop += 1
+
+            state = queue[0]
+            queue = queue[1:]
+
+            if loop != 2:
+                # stop go back to begin
+                if state.isBegin():
+                    continue
+
+            try:
+                if state.isSplit():
+                    new_states = [deepcopy(state) for i in range(8)]
+                else:
+                    new_states = [deepcopy(state) for i in range(4)]
+                # print("[+] Deep copied")
+            except KeyboardInterrupt:
+                print("\nDeep coping from:")
+                print("Ctrl+C again to break")
+                try:
+                    input(state)
+                except KeyboardInterrupt:
+                    break
+                print("\033[1H", end="")
+                print("\033[J", end="")
+
+            for active in range(2 if state.isSplit() else 1):
+                for i in range(4):
+                    try:
+                        new_states[i + 4 * active].setActiveBlock(active + 1)
+                        m = moves(i + 1)
+                        try:
+                            # don't make a reverse move
+                            if m.isReverse(new_states[i].moves[-1]):
+                                # print("{} reverse {}".format(m, new_states[i].moves[-1]))
+                                continue
+                        except IndexError:
+                            # first loop has no moves
+                            pass
+                        move(new_states[i], m)
+                        print(m)
+                        print(new_states[i])
+
+                        # check already have
+
+                        if new_states[i].isGoal():
+                            s.goal = new_states[i]
+                            print()
+                            return
+
+                    except KeyboardInterrupt:
+                        print("\nChecking state:")
+                        input(new_states[i])
+                        print("\033[1H", end="")
+                        print("\033[J", end="")
+
+                    except Exception:
+                        # invalid move
+                        continue
+
+                    queue.append(new_states[i])
+
+        except KeyboardInterrupt:
+            print("\nInterupted in unexpected code")
+            input("Sometimes this happens, try breaking again")
+            print("\033[1H", end="")
+            print("\033[J", end="")
