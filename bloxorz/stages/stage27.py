1193e1e1a1ceba56a436aca5e8b37bc0f2e6c29b
huylenguyen806 <huylenguyen806@github.com>
2018-04-21 00:24:41 +0700

    to stage 31

--- bloxorz/stages/stage27.py
+++ bloxorz/stages/stage27.py
@@ -6,9 +6,45 @@ from bloxorz.game.TileType import TileType as T
 
 
 def stage27():
+    SB1 = Tile(T.bridge, True)
+    SB2 = Tile(T.bridge, True)
+
     arr = [[] for i in range(10)]  # type: List[List[Tile]]
 
-    arr[0] = []
+    arr[0] = [Tile(), Tile(), Tile(), None, None, None, None]
+    arr[0] += [Tile() for i in range(8)]
+    
+    arr[1] = [Tile() for i in range(11)]
+    arr[1] += [None, None, Tile(), Tile()]
+
+    arr[2] = [Tile(), Tile(), Tile(), None, None, None, None, 
+                Tile(), Tile(), None, None, None, None, Tile(), Tile()]
+
+    arr[3] = [None for i in range(12)]
+    arr[3] += [Tile(), Tile(T.hard_button, [], [SB1, SB2]), Tile()]
+
+    arr[4] = [None for i in range(12)]
+    arr[4] += [Tile(), Tile()]
+
+    arr[5] = [Tile(), Tile(), Tile(), None, None, 
+                Tile(T.soft_ground), Tile(T.soft_ground), Tile(T.soft_ground), 
+                Tile(T.soft_ground), Tile(), None, None, 
+                Tile(T.soft_button, [], [SB1]), Tile(T.soft_button, [], [SB2])]
+
+    arr[6] = [Tile(), Tile(T.goal), Tile()]
+    arr[6] += [Tile(T.soft_ground) for i in range(7)]
+    arr[6] += [None, None, Tile(), Tile(), Tile()]
+
+    arr[7] = [Tile(), Tile(), Tile()]
+    arr[7] += [Tile(T.soft_ground) for i in range(9)]
+    arr[7] += [Tile(), Tile(), Tile()]
+
+    arr[8] = [None for i in range(5)]
+    arr[8] += [Tile(T.soft_ground) for i in range(7)]
+    arr[8] += [Tile(), Tile(), Tile()]
+
+    arr[9] = [None for i in range(6)]
+    arr[9] += [SB1, Tile(), Tile(), SB2]
 
     s = Stage('stage27', arr, 1, 1)
     s.save('stage27')
--- bloxorz/stages/stage28.py
+++ bloxorz/stages/stage28.py
@@ -6,7 +6,44 @@ from bloxorz.game.TileType import TileType as T
 
 
 def stage28():
-    arr = [[] for i in range(0)]  # type: List[List[Tile]]
+    B1 = [Tile(T.bridge, True), Tile(T.bridge, True)]
+    B2 = [Tile(T.bridge, True), Tile(T.bridge, True)]
 
-    s = Stage('stage28', arr, 1, 1)
+    arr = [[] for i in range(10)]  # type: List[List[Tile]]
+
+    arr[0] = [None, Tile(), Tile(), B1[0], B1[1], Tile(), Tile()]
+
+    arr[1] = [None, Tile(), Tile(), None, None, Tile(), Tile(), Tile()]
+
+    arr[2] = [Tile(T.soft_ground), Tile(T.soft_ground), Tile(), 
+                None, None, Tile(), Tile(), Tile(), Tile()]
+
+    arr[3] = [Tile(T.soft_ground), Tile(T.soft_ground)]
+    arr[3] += [None for i in range(5)]
+    arr[3] += [Tile(), Tile(), Tile()]
+
+    arr[4] = [Tile(T.soft_ground), Tile(T.soft_ground)]
+    arr[4] += [None for i in range(6)]
+    arr[4] += [Tile(), Tile(), Tile()]
+
+    arr[5] = [Tile(T.soft_ground), Tile(), Tile(), Tile()]
+    arr[5] += [None for i in range(5)]
+    arr[5] += [Tile(), Tile(), Tile(T.split, [6,14,9,12])]
+
+    arr[6] = [None, Tile(), Tile(T.goal), Tile()]
+    arr[6] += [None for i in range(6)]
+    arr[6] += [Tile() for i in range(5)]
+
+    arr[7] = [None]
+    arr[7] += [Tile() for i in range(6)]
+    arr[7] += [None, None, None, Tile(), Tile(T.soft_button, [], [B1[0], B1[1], B2[0], B2[1]]), 
+                Tile(), Tile(), Tile()]
+
+    arr[8] = [None, None, Tile(), None, None, 
+                Tile(), Tile(), None, None, None, Tile(), Tile(), Tile()]
+
+    arr[9] = [None, None, Tile(), None, None, 
+                Tile(), Tile(), Tile(), B2[0], B2[1], Tile(), Tile(), Tile()]
+
+    s = Stage('stage28', arr, 2, 2)
     s.save('stage28')
